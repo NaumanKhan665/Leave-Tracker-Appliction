33815fa2eca4fe6966830528d4f6bf08910ad809
Nauman Tasaduq <naumantasaduq665@gmail.com>
2025-02-06 18:23:25 +0500

    add edit and Add button on my leaves section

--- myLeaves/myLeaves.js
+++ myLeaves/myLeaves.js
@@ -1,4 +1,5 @@
 import { LightningElement,api } from 'lwc';
+
 const columns = [
   
     { label: 'Request Id', fieldName: 'Name' },
@@ -7,10 +8,41 @@ const columns = [
     { label: 'Reason', fieldName: 'Reson__c'},
     { label: 'Status', fieldName: 'Status__c', type: 'picklist' },
     { label: 'Manager Comments', fieldName: 'Manager_Comment__c'},
+    {
+        type: 'button',
+        typeAttributes: {
+         label: 'Edit ',
+          name: 'edit'
+        }
+      }
 ];
 export default class MyLeaves extends LightningElement {
  columns = columns;
 @api showData=[];
+isModalPopupdisabled=false;
+objectApiName="LeaveRequest__c";
+recordId='';
 
+handleRowAction(event)
+{
+
+    this.isModalPopupdisabled=true;
+ // event.detail.row;
+ this.recordId=event.detail.row.Id;
+
+
+
+
+}
+closePopupHandler(){
+
+    this.isModalPopupdisabled=false;
+}
+
+succesFormHandler()
+{
+    this.isModalPopupdisabled=false;
+
+}
 
 }
